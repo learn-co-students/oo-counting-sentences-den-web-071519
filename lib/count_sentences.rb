4d6999221ad219d95ea4b007b1637f998aa9fb89
SwanHub <jdprince555@gmail.com>
2019-07-17 12:15:52 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,18 +16,11 @@ class String
 
   def count_sentences
     array_of_indexes = []
-    array_of_sentences = []
-    array_of_words = self.split
-    array_of_words.each_with_index do |word, i|
+    self.split.each_with_index do |word, i|
       if word.end_with?('!') || word.end_with?('.') || word.end_with?('?')
         array_of_indexes << i
       end
     end
-    starting_index = 0
-    array_of_indexes.each do |retrieved_index_number|
-      array_of_sentences << array_of_words[starting_index..retrieved_index_number]
-      starting_index = retrieved_index_number + 1
-    end
-    array_of_sentences.count
+    array_of_indexes.count
   end
 end
