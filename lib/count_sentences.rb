eef3eba3e695ba3df5c4ec9f3b1743ab5454682e
SwanHub <jdprince555@gmail.com>
2019-07-16 22:03:51 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    array_of_indexes = []
+    array_of_sentences = []
+    array_of_words = self.split
+    array_of_words.each_with_index do |word, i|
+      if word.end_with?('!') || word.end_with?('.') || word.end_with?('?')
+        array_of_indexes << i
+      end
+    end
+    starting_index = 0
+    array_of_indexes.each do |retrieved_index_number|
+      array_of_sentences << array_of_words[starting_index..retrieved_index_number]
+      starting_index = retrieved_index_number + 1
+    end
+    array_of_sentences.count
   end
 end
